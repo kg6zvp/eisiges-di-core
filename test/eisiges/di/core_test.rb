46f033d16a6c939c66f3a8c8a7565df42e1c4df7
Sam McCollum <smccollum@mccollum.enterprises>
2018-02-27 16:28:30 -0800

    sm - create proper test

--- test/eisiges/di/core_test.rb
+++ test/eisiges/di/core_test.rb
@@ -1,11 +1,25 @@
 require 'test_helper'
 
-class Eisiges::DI::CoreTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Eisiges::DI::Core::VERSION
-  end
+class ServiceObject
+	def get_Status
+		"status: green"
+	end
+end
 
-  def test_it_does_something_useful
-    assert false
-  end
+class DiExample
+	include Eisiges::DI::Core
+
+	inject klasse: ServiceObject, as: :so
+end
+	
+
+class Eisiges::DI::CoreTest < Minitest::Test
+	def test__that_it_has_a_version_number
+		refute_nil ::Eisiges::DI::Core::VERSION
+	end
+
+	def test__it_can_list_dependencies_in_example
+		assert_equal({:so => ServiceObject}, DiExample.dependencies)
+		assert_equal({:so => ServiceObject}, DiExample.get_injection_points)
+	end
 end
