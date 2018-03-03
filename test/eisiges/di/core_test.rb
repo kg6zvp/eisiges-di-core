484d81001dd60fa5b749335531a2705b22f2edf2
Sam McCollum <smccollum@mccollum.enterprises>
2018-03-02 22:55:02 -0800

    sm - repair auto  mixin and improve tests

--- test/eisiges/di/core_test.rb
+++ test/eisiges/di/core_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ServiceObject
+	shareable :my_scope
+
 	def get_Status
 		"status: green"
 	end
@@ -21,5 +23,16 @@ class Eisiges::DI::CoreTest < Minitest::Test
 	def test__it_can_list_dependencies_in_example
 		assert_equal({:so => ServiceObject}, DiExample.dependencies)
 		assert_equal({:so => ServiceObject}, DiExample.get_injection_points)
+
+		assert_equal({}, ServiceObject.dependencies)
+		assert_equal({}, ServiceObject.get_injection_points)
+	end
+
+	def test__it_can_express_shareability
+		refute_nil DiExample.shareable?
+		refute_nil ServiceObject.shareable?
+
+		refute DiExample.shareable?
+		assert_equal :my_scope, ServiceObject.shareable?
 	end
 end
